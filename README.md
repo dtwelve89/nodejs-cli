5d44e273b13a95f91f50a38c5181a551e1e835c7
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-12-10 22:01:47 -0800

    Upload submission

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# nodejs-cli
+## NodeCLI Coding Challenge by Danny Nguyen
+
+### Summary:
+
+A modern twist on the classic Unix list(ls) command developed in NodeJS.
+
+The client is able to run the `ls` command in their CLI to show all files/folders in the current directory. Additionally, running the `ls -R` will show all files/folders in the current directory and subdirectories as well.
+
+An error will prompt if an invalid command is entered.
+
+### Technologies Used:
+
+JavaScript, NodeJS
+
+### Run Instructions:
+
+- `node . ls` - show all files/folders in the current directory
+- `node . ls -R`- show all files/folders in the current directory and subdirectories
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+const initializeApp = () => {
+  // Exit if no arguments passed
+  if (process.argv.length <= 2) {
+    process.exit(-1);
+  }
+
+  if (arg1 === 'ls' && !arg2) {
+    list();
+  } else if (arg1 === 'ls' && arg2 === '-R') {
+    listWithSub();
+  } else {
+    console.log(`zsh: command not found: ${arg1}`);
+  }
+};
+
+// Show files/folders in your current directory
+const list = (dir = './') => {
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+    // Filter out hidden files with Regex
+    files = files.filter(item => !/(^|\/)\.[^\/\.]/g.test(item));
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+};
+
+// Show all files/folders in directory and subdirectories
+const listWithSub = (dir = './', files) => {
+  files = fs.readdirSync(dir);
+  files = files.filter(item => !/(^|\/)\.[^\/\.]/g.test(item));
+
+  files.forEach(file => {
+    console.log(file);
+  });
+
+  files.forEach(file => {
+    let dirPath = path.join(dir, file);
+    if (fs.statSync(dirPath).isDirectory()) {
+      console.log(`\n./${dirPath}:`);
+      listWithSub(dirPath, files);
+    }
+  });
+};
+
+initializeApp();
